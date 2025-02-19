fd017c67d5a82b06c2c4880090f6265dc838b9ba
yow-cow <charlesyow@gmail.com>
2025-02-19 12:44:34 -0600

    Initial Commit

---
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Charles Yow on 2/19/25.
+//
+
+import Testing
+@testable import CurrencyConverter
+
+struct CurrencyConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
